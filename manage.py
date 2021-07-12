ceabc44bfcfe42923678fb2fb759b3b377555208
Keerthanamunna <77993645+Keerthanamunna@users.noreply.github.com>
2021-07-12 01:15:55 -0400

    code is done

--- manage.py
+++ manage.py
@@ -1,4 +1,4 @@
-'''from flask import app
+from flask import app
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from app import create_app
@@ -12,4 +12,4 @@ manager.add_command('db', MigrateCommand)
 
 
 if __name__ == '__main__':
-    manager.run()'''
+    manager.run()
